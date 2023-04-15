387f35c996c424c198ae40c5051cf8dd9b6f0a8d
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-15 16:41:51 +0530

    Update aniquote.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/modules/aniquote.py
+++ FallenRobot/modules/aniquote.py
@@ -1,23 +1,12 @@
 import requests
 from telegram import ParseMode, Update
+from telegram.ext import CommandHandler, CallbackContext, run_async
 from FallenRobot.events import register
-from telegram.ext import CallbackContext, run_async
-from pyrogram import __version__ as pyrover
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
 
-@pbot.on_message(filters.command("quote"))
 @run_async
 def quote(update: Update, context: CallbackContext):
-    message = update.effective_message
-    text = message.text.split(" ", 1)
-    if len(text) == 1:
-        r = requests.get("https://animechan.vercel.app/api/random").json()
-        reply_text = f"**{r['quote']}**\n"
-    else:
-        variabla = text[1]
-        r = requests.get(f"https://animechan.vercel.app/api/random").json()
-        reply_text = f"**{r['quote']}**\n"
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    r = requests.get("https://animechan.vercel.app/api/random").json()
+    reply_text = f"**{r['quote']}** - {r['character']} ({r['anime']})"
+    update.message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+register("quote", quote)
