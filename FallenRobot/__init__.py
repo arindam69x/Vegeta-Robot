b5fcff9fa149d3984a3c2e706d369a8e2e5d3b6c
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-15 16:26:09 +0530

    Update __init__.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -10,20 +10,7 @@ from telethon import TelegramClient
 from telegraph import Telegraph
 
 StartTime = time.time()
-
-
-import logging
-from FallenRobot.config import Development as Config
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('log.txt'),
-              logging.StreamHandler()],
-    level=logging.INFO)
-
-TOKEN = Config.TOKEN
-
-              
+            
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
