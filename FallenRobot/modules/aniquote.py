ecc92ca2d45188eca82f1e3deff6323b196e6f22
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-15 15:27:42 +0530

    Update aniquote.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/modules/aniquote.py
+++ FallenRobot/modules/aniquote.py
@@ -1,25 +1,12 @@
-import requests
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
-
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-
-
 @run_async
 def quote(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://animechan.vercel.app/api/random").json()
-        reply_text = f"**{resp['quote']}**\n"
+        reply_text = f"**{r['quote']}**\n"
     else:
         variabla = text[1]
         r = requests.get(f"https://animechan.vercel.app/api/random").json()
-        reply_text = f"**{resp['quote']}**\n"
+        reply_text = f"**{r['quote']}**\n"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
-    
-    """
-QUOTES_HANDLER = DisableAbleCommandHandler("quote", quote)
-
-dispatcher.add_handler(QUOTES_HANDLER)
