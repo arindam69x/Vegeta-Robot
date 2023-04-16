1e752ff2538d40ee92c9bc83213ed5008bbf7f4a
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-16 19:02:27 +0530

    Update and rename anime_quote.py to quote.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

---
+++ FallenRobot/modules/quote.py
@@ -0,0 +1,35 @@
+import requests
+import html
+
+from telegram import Update, ParseMode
+from telegram.ext import CommandHandler, CallbackContext
+
+def quote(update: Update, context: CallbackContext):
+    res = requests.get('https://animechan.vercel.app/api/random')
+    if res.status_code == 200:
+        data = res.json()
+        quote = html.escape(data['quote'])
+        anime = html.escape(data['anime'])
+        character = html.escape(data['character'])
+        message = f'<i>{quote}</i>\n\n<b>{anime}</b>\n{character}'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode=ParseMode.HTML)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, failed to fetch the quote. Please try again later.")
+    
+
+QUOTE_HANDLER = CommandHandler("quote", quote)
+
+def __stats__():
+    return f"- {len(anime_quote_strings.QUOTES)} quotes in {len(anime_quote_strings.ANIME_LIST)} anime series."
+    
+
+__help__ = f"""{__stats__()}
+*Commands:*
+- /quote: Get a random anime quote.
+"""
+
+__mod_name__ = "Anime Quotes"
+
+__handlers__ = [
+    QUOTE_HANDLER,
+]
