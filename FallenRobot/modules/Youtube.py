8d53700bb71aeb931493ed59292d1c120643a4d6
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-16 22:24:07 +0530

    Update Youtube.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/modules/Youtube.py
+++ FallenRobot/modules/Youtube.py
@@ -1,85 +1,47 @@
-import logging
-import aiohttp
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
-from youtube_dl import YoutubeDL
-from FallenRobot import dispatcher
+import telebot
+from pytube import YouTube
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+bot = telebot.TeleBot('6150409031:AAF31Y9CsAnYgvLcALZpi23JXgYuBt5lsqw')
 
-# Define the states of the conversation
-VIDEO, AUDIO = range(2)
+@bot.message_handler(commands=['yt'])
+def download_video(message):
+    chat_id = message.chat.id
+    msg = bot.send_message(chat_id, "Do you want to download a video or audio? Please type 'video' or 'audio'.")
+    bot.register_next_step_handler(msg, process_type_step)
 
-# Define the callback functions for the conversation
-def video_callback(update: Update, context: CallbackContext) -> int:
-    # Send a message to the user
-    update.message.reply_text('Please enter the URL of the video you want to download.')
+def process_type_step(message):
+    chat_id = message.chat.id
+    user_choice = message.text.lower()
+    if user_choice == 'video':
+        msg = bot.send_message(chat_id, "Please enter the video link:")
+        bot.register_next_step_handler(msg, process_video_step)
+    elif user_choice == 'audio':
+        msg = bot.send_message(chat_id, "Please enter the video link:")
+        bot.register_next_step_handler(msg, process_audio_step)
+    else:
+        bot.send_message(chat_id, "Invalid input. Please type 'video' or 'audio'.")
+        bot.register_next_step_handler(msg, process_type_step)
 
-    # Set the state of the conversation to VIDEO
-    return VIDEO
+def process_video_step(message):
+    chat_id = message.chat.id
+    link = message.text
+    try:
+        video = YouTube(link)
+        stream = video.streams.get_highest_resolution()
+        stream.download()
+        bot.send_message(chat_id, "Video downloaded successfully!")
+    except Exception as e:
+        bot.send_message(chat_id, f"An error occurred: {str(e)}")
 
-def audio_callback(update: Update, context: CallbackContext) -> int:
-    # Send a message to the user
-    update.message.reply_text('Please enter the URL of the video you want to extract audio from.')
+def process_audio_step(message):
+    chat_id = message.chat.id
+    link = message.text
+    try:
+        video = YouTube(link)
+        stream = video.streams.filter(only_audio=True).first()
+        stream.download()
+        bot.send_message(chat_id, "Audio downloaded successfully!")
+    except Exception as e:
+        bot.send_message(chat_id, f"An error occurred: {str(e)}")
 
-    # Set the state of the conversation to AUDIO
-    return AUDIO
-
-def video_url(update: Update, context: CallbackContext) -> int:
-    # Get the URL from the user's message
-    url = update.message.text
-
-    # Download the video using youtube_dl
-    ydl_opts = {'outtmpl': 'video.mp4'}
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    # Send the video to the user
-    with open('video.mp4', 'rb') as video_file:
-        update.message.reply_video(video=video_file)
-
-    # End the conversation
-    return ConversationHandler.END
-
-def audio_url(update: Update, context: CallbackContext) -> int:
-    # Get the URL from the user's message
-    url = update.message.text
-
-    # Download the audio using youtube_dl
-    ydl_opts = {'outtmpl': 'audio.mp3', 'format': 'bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]}
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    # Send the audio to the user
-    with open('audio.mp3', 'rb') as audio_file:
-        update.message.reply_audio(audio=audio_file)
-
-    # End the conversation
-    return ConversationHandler.END
-
-def cancel(update: Update, context: CallbackContext) -> int:
-    # Send a message to the user indicating that the conversation has been canceled
-    update.message.reply_text('Conversation canceled.')
-
-    # End the conversation
-    return ConversationHandler.END
-
-def youtube_handler(update: Update, context: CallbackContext) -> None:
-    # Start the conversation
-    update.message.reply_text('Do you want to download a video or extract audio? Type /cancel to stop.')
-
-    # Set up the ConversationHandler
-    yt_handler = ConversationHandler(
-        entry_points=[CommandHandler('yt', video_callback)],
-        states={
-            VIDEO: [MessageHandler(Filters.text & ~Filters.command, video_url)],
-            AUDIO: [MessageHandler(Filters.text & ~Filters.command, audio_url)]
-        },
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
-
-    # Add the ConversationHandler to the dispatcher
-    updater.dispatcher.add_handler(yt_handler)
-
+bot.polling()
