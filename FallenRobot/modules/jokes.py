8c11a9024cc0866acfeae4b72c71859797d99c77
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-16 19:30:34 +0530

    Update jokes.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/modules/jokes.py
+++ FallenRobot/modules/jokes.py
@@ -1,28 +1,22 @@
 import requests
-from telegram import Update
+
+from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler
 
-def joke(update: Update, context: CallbackContext):
-    response = requests.get("https://official-joke-api.appspot.com/random_joke")
+from FallenRobot import dispatcher
+
+
+def jokes(update: Update, context: CallbackContext):
+    url = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        setup = data['setup']
-        punchline = data['punchline']
-        update.message.reply_text(setup)
-        context.job_queue.run_once(send_punchline, 5, context=[punchline, update.message.chat_id])
+        joke = f"<b>{data['setup']}</b>\n\n{data['punchline']}"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=joke, parse_mode=ParseMode.HTML)
     else:
-        update.message.reply_text("Sorry, something went wrong.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Oops, something went wrong. Please try again later.")
 
-def send_punchline(context):
-    punchline, chat_id = context.job.context
-    context.bot.send_message(chat_id=chat_id, text=punchline)
 
-__help__ = """
-Get a random joke by using the command /joke
-"""
+jokes_handler = CommandHandler("jokes", jokes)
+dispatcher.add_handler(jokes_handler)
 
-__mod_name__ = "Jokes"
-
-JOKES_HANDLER = CommandHandler("joke", joke)
-
-dispatcher.add_handler(JOKES_HANDLER)
