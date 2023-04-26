def1046dc69bfbc48b9ea31bd7a8426fee5a03c0
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-26 10:06:04 +0530

    Update disable.py
    Signed-off-by: || ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>

--- FallenRobot/modules/disable.py
+++ FallenRobot/modules/disable.py
@@ -347,7 +347,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "Dɪsᴀʙʟᴇ"
+    __mod_name__ = "Disable"
 
 else:
     DisableAbleCommandHandler = CommandHandler
