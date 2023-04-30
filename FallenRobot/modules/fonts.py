0e82a16cf239917d0b9e286d6512c216358d2580
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-30 09:38:48 +0530

    Update fonts.py
    Imfao 
    
    Signed-off-by: || ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>

--- FallenRobot/modules/fonts.py
+++ FallenRobot/modules/fonts.py
@@ -187,7 +187,7 @@ async def style(c, m):
         cls = Fonts.strike
     if style == "frozen":
         cls = Fonts.frozen
-    new_text = cls(m.message.reply_to_message.text)
+    new_text = cls(m.message.text)
     try:
         return await m.message.edit_text(new_text, reply_markup=m.message.reply_markup)
     except:
