ee5587a4d6e6897521b64b1aba017e6c9d46fee7
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2023-05-02 11:57:18 +0530

    Update ud.py
    Signed-off-by: Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>

--- FallenRobot/modules/ud.py
+++ FallenRobot/modules/ud.py
@@ -8,7 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_message(filters.command("ud"))
 async def urban(_, m):  
-       global mm
        user_id = m.from_user.id
        if len(m.text.split()) == 1:
          return await m.reply("Enter the text for which you would like to find the definition.")
@@ -18,25 +17,35 @@ async def urban(_, m):
        if 0 == len(mm):
            return await m.reply("=> No results Found!")
        string = f"🔍 **Ward**: {mm[0].get('word')}\n\n📝 **Definition**: {mm[0].get('definition')}\n\n✏️ **Example**: {mm[0].get('example')}"
-       return await m.reply(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('next', callback_data=f"udnxt:1:{user_id}")]]), quote=True)
-              
+       if 1 == len(mm):
+           return await m.reply(text=string, quote=True)
+       else:
+           num = 0
+           return await m.reply(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('next', callback_data=f"udnxt:{user_id}:{text}:{num}")]]), quote=True)
 @app.on_callback_query(filters.regex("^udnxt"))   
 async def next(_, query):
-       user_id = query.from_user.id
-       if not user_id == int(query.data.split(":")[-1]):
+         user_id = int(query.data.split(":")[1])
+         text = str(query.data.split(":")[2])
+         num = int(query.data.split(":")[3])+1
+         if not query.from_user.id == user_id:
              return await query.answer("This is not for You!")
-       num = int(query.data.split(":")[1])+1
-       uwu = mm[num]
-       if num == len(mm)-1:
-       	  return await query.message.edit('=> No more definition could be found!')
-       string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}"
-       return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('➡️ Next', callback_data=f"udnxt:{num}:{user_id}")]]))
-       
+         api = requests.get(f"https://api.urbandictionary.com/v0/define?term={text}").json()
+         mm = api["list"]
+         uwu = mm[num]
+         if num == len(mm)-1:
+             string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}\n\n"
+             string += "No More Results"
+             return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('➡️ Back', callback_data=f"udbck:{query.from_user.id}:{text}:{num}")]]))
+         else:
+             buttons = [[
+                  InlineKeyboardButton("Back ⏮️", callback_data=f"udbck:{query.from_user.id}:{text}:{num}"),
+                  InlineKeyboardButton("Next ⏭️", callback_data=f"udnxt:{query.from_user.id}:{text}:{num}") 
+             ]]
        
        
 
 
-
+       
 __help__ = """
 » /ud (text) *:* Searchs the given text on Urban Dictionary and sends you the information.
 """
